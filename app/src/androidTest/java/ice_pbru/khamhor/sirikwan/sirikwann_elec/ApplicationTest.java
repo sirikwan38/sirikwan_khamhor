d4d4c409e5cf9ff5feff9ccd3a7d2118aa2d2433
sirikwan <k.sirikwann@gmail.com>
2016-04-21 12:54:11 +0700

    test

---
+++ app/src/androidTest/java/ice_pbru/khamhor/sirikwan/sirikwann_elec/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ice_pbru.khamhor.sirikwan.sirikwann_elec;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
